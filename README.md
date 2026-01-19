b613c07079a72185b9d7049be75b6233b356ba1f
Roman Phasikawa <romanphasikawa@gmail.com>
2026-01-19 14:47:33 +0545

    Fix formatting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+| SN. | Topic |
+| :--- | :--- |
 | 01 | [Basic](./01-Basic) |
 | 02 | [TypeCasting](./02-TypeCasting) |
---
+++ lab-reports/Lab1-IntroductionToJava/src/WelcomeMessage.java
@@ -0,0 +1,6 @@
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming");
+    }
+
+}
